41d7ef293d930a5691fc2e46d8ad37d3452762f8
John Stahl <john.stahl24@gmail.com>
2018-10-09 03:09:12 +0000

    added in the lessons information

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :courses, only: [:index, :show]
   namespace :instructor do
-    resources :courses, only: [:new, :create, :show]
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
+    resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
+    end
   end
 end
